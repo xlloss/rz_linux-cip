309ad2e2295fe086950c7a871c68deeab3cddd9a
slash <slash@gmail.com>
2024-10-30 18:49:18 +0800

    [AUDIO_CM7104] add playback function

--- sound/soc/codecs/cm7104_48k.c
+++ sound/soc/codecs/cm7104_48k.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /* Copyright (c) 2010-2011,2013-2015 The Linux Foundation. All rights reserved.
  *
- * max98357a.c -- MAX98357A ALSA SoC Codec driver
+ * cm7104_48k.c -- cm7104 ALSA SoC Codec driver
  */
 
 #include <linux/acpi.h>
@@ -20,165 +20,157 @@
 #include <sound/soc-dai.h>
 #include <sound/soc-dapm.h>
 
-struct max98357a_priv {
+#define USE_GPIO_SDMODE 0
+
+struct cm7104_priv {
+	struct gpio_desc *hp_dev;
+	#if USE_GPIO_SDMODE
 	struct gpio_desc *sdmode;
-	unsigned int sdmode_delay;
+	#endif
 	int sdmode_switch;
 };
 
-static int max98357a_daiops_trigger(struct snd_pcm_substream *substream,
+static int cm7104_daiops_trigger(struct snd_pcm_substream *substream,
 		int cmd, struct snd_soc_dai *dai)
 {
 	struct snd_soc_component *component = dai->component;
-	struct max98357a_priv *max98357a =
+	#if USE_GPIO_SDMODE
+	struct cm7104_priv *cm7104 =
 		snd_soc_component_get_drvdata(component);
 
-	if (!max98357a->sdmode)
+	if (!cm7104->sdmode)
 		return 0;
-
+	#endif
 	switch (cmd) {
 	case SNDRV_PCM_TRIGGER_START:
 	case SNDRV_PCM_TRIGGER_RESUME:
 	case SNDRV_PCM_TRIGGER_PAUSE_RELEASE:
-		mdelay(max98357a->sdmode_delay);
-		if (max98357a->sdmode_switch) {
-			gpiod_set_value(max98357a->sdmode, 1);
-			dev_dbg(component->dev, "set sdmode to 1");
-		}
+		dev_info(component->dev, "START [cmd %d]\n", cmd);
 		break;
 	case SNDRV_PCM_TRIGGER_STOP:
 	case SNDRV_PCM_TRIGGER_SUSPEND:
 	case SNDRV_PCM_TRIGGER_PAUSE_PUSH:
-		gpiod_set_value(max98357a->sdmode, 0);
-		dev_dbg(component->dev, "set sdmode to 0");
+		dev_info(component->dev, "STOP [cmd %d]\n", cmd);
 		break;
 	}
 
 	return 0;
 }
 
-static int max98357a_sdmode_event(struct snd_soc_dapm_widget *w,
+static int cm7104_sdmode_event(struct snd_soc_dapm_widget *w,
 		struct snd_kcontrol *kcontrol, int event)
 {
 	struct snd_soc_component *component =
 		snd_soc_dapm_to_component(w->dapm);
-	struct max98357a_priv *max98357a =
+	struct cm7104_priv *cm7104 =
 		snd_soc_component_get_drvdata(component);
 
 	if (event & SND_SOC_DAPM_POST_PMU)
-		max98357a->sdmode_switch = 1;
+		cm7104->sdmode_switch = 1;
 	else if (event & SND_SOC_DAPM_POST_PMD)
-		max98357a->sdmode_switch = 0;
+		cm7104->sdmode_switch = 0;
 
 	return 0;
 }
 
-static const struct snd_soc_dapm_widget max98357a_dapm_widgets[] = {
+static const struct snd_soc_dapm_widget cm7104_dapm_widgets[] = {
 	SND_SOC_DAPM_OUTPUT("Speaker"),
 	SND_SOC_DAPM_OUT_DRV_E("SD_MODE", SND_SOC_NOPM, 0, 0, NULL, 0,
-			max98357a_sdmode_event,
+			cm7104_sdmode_event,
 			SND_SOC_DAPM_POST_PMU | SND_SOC_DAPM_POST_PMD),
 };
 
-static const struct snd_soc_dapm_route max98357a_dapm_routes[] = {
+static const struct snd_soc_dapm_route cm7104_dapm_routes[] = {
 	{"SD_MODE", NULL, "HiFi Playback"},
 	{"Speaker", NULL, "SD_MODE"},
 };
 
-static const struct snd_soc_component_driver max98357a_component_driver = {
-	.dapm_widgets		= max98357a_dapm_widgets,
-	.num_dapm_widgets	= ARRAY_SIZE(max98357a_dapm_widgets),
-	.dapm_routes		= max98357a_dapm_routes,
-	.num_dapm_routes	= ARRAY_SIZE(max98357a_dapm_routes),
+static const struct snd_soc_component_driver cm7104_component_driver = {
+	.dapm_widgets		= cm7104_dapm_widgets,
+	.num_dapm_widgets	= ARRAY_SIZE(cm7104_dapm_widgets),
+	.dapm_routes		= cm7104_dapm_routes,
+	.num_dapm_routes	= ARRAY_SIZE(cm7104_dapm_routes),
 	.idle_bias_on		= 1,
 	.use_pmdown_time	= 1,
 	.endianness		= 1,
 	.non_legacy_dai_naming	= 1,
 };
 
-static const struct snd_soc_dai_ops max98357a_dai_ops = {
-	.trigger        = max98357a_daiops_trigger,
+static const struct snd_soc_dai_ops cm7104_dai_ops = {
+	.trigger        = cm7104_daiops_trigger,
 };
 
-static struct snd_soc_dai_driver max98357a_dai_driver = {
-	.name = "HiFi",
+static struct snd_soc_dai_driver cm7104_dai_driver = {
+	.name = "CM7104-HiFi",
 	.playback = {
 		.stream_name	= "HiFi Playback",
 		.formats	= SNDRV_PCM_FMTBIT_S16 |
 					SNDRV_PCM_FMTBIT_S24 |
 					SNDRV_PCM_FMTBIT_S32,
-		.rates		= SNDRV_PCM_RATE_8000 |
-					SNDRV_PCM_RATE_16000 |
-					SNDRV_PCM_RATE_32000 |
-					SNDRV_PCM_RATE_44100 |
-					SNDRV_PCM_RATE_48000 |
-					SNDRV_PCM_RATE_88200 |
-					SNDRV_PCM_RATE_96000,
-		.rate_min	= 8000,
-		.rate_max	= 96000,
+		.rates		= SNDRV_PCM_RATE_48000,
+		.rate_min	= 48000,
+		.rate_max	= 48000,
 		.channels_min	= 1,
 		.channels_max	= 2,
 	},
-	.ops    = &max98357a_dai_ops,
+	.ops    = &cm7104_dai_ops,
 };
 
-static int max98357a_platform_probe(struct platform_device *pdev)
+static int cm7104_platform_probe(struct platform_device *pdev)
 {
-	struct max98357a_priv *max98357a;
-	int ret;
-
-	max98357a = devm_kzalloc(&pdev->dev, sizeof(*max98357a), GFP_KERNEL);
-	if (!max98357a)
+	struct cm7104_priv *cm7104;
+	pr_info("%s %d\n", __func__, __LINE__);
+	cm7104 = devm_kzalloc(&pdev->dev, sizeof(*cm7104), GFP_KERNEL);
+	if (!cm7104)
 		return -ENOMEM;
-
-	max98357a->sdmode = devm_gpiod_get_optional(&pdev->dev,
+	#if USE_GPIO_SDMODE
+	cm7104->sdmode = devm_gpiod_get_optional(&pdev->dev,
 				"sdmode", GPIOD_OUT_LOW);
-	if (IS_ERR(max98357a->sdmode))
-		return PTR_ERR(max98357a->sdmode);
+	if (IS_ERR(cm7104->sdmode))
+		return PTR_ERR(cm7104->sdmode);
 
 	ret = device_property_read_u32(&pdev->dev, "sdmode-delay",
-					&max98357a->sdmode_delay);
+					&cm7104->sdmode_delay);
 	if (ret) {
-		max98357a->sdmode_delay = 0;
+		cm7104->sdmode_delay = 0;
 		dev_dbg(&pdev->dev,
 			"no optional property 'sdmode-delay' found, "
 			"default: no delay\n");
 	}
+	#endif
 
-	dev_set_drvdata(&pdev->dev, max98357a);
+	dev_set_drvdata(&pdev->dev, cm7104);
 
 	return devm_snd_soc_register_component(&pdev->dev,
-			&max98357a_component_driver,
-			&max98357a_dai_driver, 1);
+			&cm7104_component_driver,
+			&cm7104_dai_driver, 1);
 }
 
 #ifdef CONFIG_OF
-static const struct of_device_id max98357a_device_id[] = {
-	{ .compatible = "maxim,max98357a" },
-	{ .compatible = "maxim,max98360a" },
+static const struct of_device_id cm7104_device_id[] = {
+	{ .compatible = "cmedia,cm7104" },
 	{}
 };
-MODULE_DEVICE_TABLE(of, max98357a_device_id);
+MODULE_DEVICE_TABLE(of, cm7104_device_id);
 #endif
 
 #ifdef CONFIG_ACPI
-static const struct acpi_device_id max98357a_acpi_match[] = {
-	{ "MX98357A", 0 },
-	{ "MX98360A", 0 },
+static const struct acpi_device_id cm7104_acpi_match[] = {
+	{ "CM7104", 0 },
 	{},
 };
-MODULE_DEVICE_TABLE(acpi, max98357a_acpi_match);
+MODULE_DEVICE_TABLE(acpi, cm7104_acpi_match);
 #endif
 
-static struct platform_driver max98357a_platform_driver = {
+static struct platform_driver cm7104_platform_driver = {
 	.driver = {
-		.name = "max98357a",
-		.of_match_table = of_match_ptr(max98357a_device_id),
-		.acpi_match_table = ACPI_PTR(max98357a_acpi_match),
+		.name = "cm7104",
+		.of_match_table = of_match_ptr(cm7104_device_id),
+		.acpi_match_table = ACPI_PTR(cm7104_acpi_match),
 	},
-	.probe	= max98357a_platform_probe,
+	.probe	= cm7104_platform_probe,
 };
-module_platform_driver(max98357a_platform_driver);
+module_platform_driver(cm7104_platform_driver);
 
-MODULE_DESCRIPTION("Maxim MAX98357A Codec Driver");
+MODULE_DESCRIPTION("CMedia CM7104 Codec Driver");
 MODULE_LICENSE("GPL v2");
